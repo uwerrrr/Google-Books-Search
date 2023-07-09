6f6b29aaeff1c8d058420ebdcdb2231adc6e1ea4
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-07-09 22:22:55 +1000

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-// import "./App.css";
 import "./App.scss";
 
 import Header from "./Components/Header/Header";
 import Main from "./Components/Main/Main";
-import Footer from "./Components/Footer/Footer";
+// import Footer from "./Components/Footer/Footer";
 
 import BookSearchContextProvider from "./context/BookSearchContextProvider";
-// import BookModalContextProvider from "./Context/BookModalContextProvider";
-// import BookSelectedContextProvider from "./Context/BookSelectedContextProvider";
 
 function App() {
   return (
@@ -17,13 +13,8 @@ function App() {
 
       <BookSearchContextProvider>
         {/* { searchTerm, handleSearch } */}
-        {/* <BookSelectedContextProvider> */}
-          {/* { bookSelected, selectBook } */}
-          {/* <BookModalContextProvider> */}
-            {/* { bookModalOpened, openBookModal, closeBookModal } */}
-            <Main />
-          {/* </BookModalContextProvider> */}
-        {/* </BookSelectedContextProvider> */}
+
+        <Main />
       </BookSearchContextProvider>
 
       {/* <Footer /> */}
