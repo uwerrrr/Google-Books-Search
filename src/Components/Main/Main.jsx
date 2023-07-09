b58873aebd20f593c4dafee11c775a8fc4a21911
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-07-09 22:39:18 +1000

    Update Main.jsx

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,18 +5,15 @@ import BookLoader from "./BookLoader/BookLoader";
 import BookModal from "./BookModal/BookModal";
 
 import { BookSearchContext } from "../../context/BookSearchContextProvider";
-import { BookModalContext } from "../../Context/BookModalContextProvider";
 
 const Main = () => {
   const { searchTerm } = useContext(BookSearchContext);
- 
 
   return (
     <main className={style.main}>
       <SearchBar />
 
       {searchTerm && <BookLoader />}
-  
     </main>
   );
 };
